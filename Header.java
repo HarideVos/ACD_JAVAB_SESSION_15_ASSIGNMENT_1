f30169efb053ddc3ce9d4be7f049e62ff993114a
HarideVos <53867807+HarideVos@users.noreply.github.com>
2019-08-29 22:57:30 -0400

    Add files via upload

---
+++ Header.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Header
+ */
+@WebServlet("/Header")
+public class Header extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Header() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		Enumeration<String> names = request.getHeaderNames();
+		String name = "";
+		while (names.hasMoreElements()) {
+			name = names.nextElement();
+			response.getWriter().append(name + " = " + request.getHeader(name) + "<br />");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Dispatcher</title>
+</head>
+<body>
+	<form action="Dispatcher" method="get">
+	<input type="submit" value="Dispatch.include()">
+	</form>
+	<form action="Dispatcher" method="post">
+	<input type="submit" value="Dispatch.forward()">
+	</form>
+</body>
+</html>
